d02de65e22e8c88e6d6d40606dd783282e8039a3
Shreyas Fatale <125130119+shreyas4114@users.noreply.github.com>
2024-08-13 00:06:38 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Shreyas#0411',
+    password: '',
     database: 'FlashQue'
   });
 
